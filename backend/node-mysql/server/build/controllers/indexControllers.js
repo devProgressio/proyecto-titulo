b83d62d3e95dd305a830769792453522242bef93
dev.progressio <dev.progressio@gmail.com>
2021-02-28 22:22:34 -0300

    Se elimina backend de ejemplo

--- backend/node-mysql/server/build/controllers/indexControllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IndexControllers {
-    index(req, res) {
-        res.json({ text: 'API Is /api/games' });
-    }
-}
-const indexControllers = new IndexControllers();
-exports.default = indexControllers;
--- backend/node-mysql/server/src/keys.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    database: {
-        host: '127.0.0.1',
-        user: 'node',
-        password: 'Nodejs123.',
-        database: 'ng_games_db' 
-    }
-}
